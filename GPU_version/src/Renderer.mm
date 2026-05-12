64dca0fc9379e7d7bebef006e8a089d0c8899252
grassEqualsBugs <eizprintz@gmail.com>
2026-05-11 18:36:58 -0700

    Compute shader test

--- GPU_version/src/Renderer.mm
+++ GPU_version/src/Renderer.mm
@@ -5,46 +5,77 @@
 
 // Actual implementation of renderer
 @implementation Renderer {
-  id<MTLDevice> _device;
-  id<MTLCommandQueue> _commandQueue;
-  id<MTLRenderPipelineState> _pipelineState;
+	id<MTLDevice> _device;
+	id<MTLCommandQueue> _commandQueue;
+	id<MTLComputePipelineState> _computePipeline;
+	id<MTLRenderPipelineState> _renderPipeline;
+	id<MTLTexture> _texture;
 }
 
-// Constructor
+// Constructor (initialization of renderer)
 - (instancetype)initWithMetalKitView:(MTKView *)view {
-    self = [super init];
-    if (self) {
-        _device = view.device;
-        _commandQueue = [_device newCommandQueue];
+  	self = [super init];
+    if (!self) return nil;
+
+    _device = view.device; // get access to the GPU
+    _commandQueue = [_device newCommandQueue]; // "conveyer belt" for command buffers per frame
+
+    // load the compiled shaders into the library of shaders
+    id<MTLLibrary> library = [_device newDefaultLibrary];
+    if (library == nil) {
+    	NSLog(@"Failed to find default library.");
+        return nil;
     }
 
-    // ----- Shaders for full screen quad ----
-    // load the compiled shaders
-    id<MTLLibrary> library = [_device newDefaultLibrary];
-    id<MTLFunction> vertexFn   = [library newFunctionWithName:@"vertex_main"];
-    id<MTLFunction> fragmentFn = [library newFunctionWithName:@"fragment_main"];
+    // ---- set up compute pipeline ----
+    id<MTLFunction> kernelFn = [library newFunctionWithName:@"compute_test"];
+    NSError *error = nil;
+    _computePipeline = [_device newComputePipelineStateWithFunction: kernelFn error: &error];
+    if (error) NSLog(@"Compute pipeline error: %@", error);
 
-    // describe the pipeline
+    // ---- set up rendering pipeline ----
+    id<MTLFunction> vertexFn = [library newFunctionWithName:@"vertex_main"];
+    id<MTLFunction> fragmentFn = [library newFunctionWithName:@"fragment_main"];
     MTLRenderPipelineDescriptor *pipeDesc = [[MTLRenderPipelineDescriptor alloc] init];
-    pipeDesc.vertexFunction   = vertexFn;
+    pipeDesc.vertexFunction = vertexFn;
     pipeDesc.fragmentFunction = fragmentFn;
     pipeDesc.colorAttachments[0].pixelFormat = view.colorPixelFormat;
+    _renderPipeline = [_device newRenderPipelineStateWithDescriptor:pipeDesc error:&error];
 
-    // compile it into a pipeline state object
-    NSError *error = nil;
-    _pipelineState = [_device newRenderPipelineStateWithDescriptor:pipeDesc error:&error];
+    // allocate texture at initialization to attribute of Renderer, to be used later in drawInMTKView
+    // data is all on gpu side, this is just to get the initial empty arrays over to gpu
+    MTLTextureDescriptor *td = [[MTLTextureDescriptor alloc] init];
+    td.textureType = MTLTextureType2D;
+    td.pixelFormat = MTLPixelFormatRGBA32Float;
+    td.width = 1600; td.height = 900;
+    td.usage = MTLTextureUsageShaderRead | MTLTextureUsageShaderWrite;
+    td.storageMode = MTLStorageModePrivate; // Store texture only on GPU. fastest
+    _texture = [_device newTextureWithDescriptor:td];
+
     return self;
 }
 
 // drawInMTKView method -- render loop for the program, called every frame
 - (void)drawInMTKView:(MTKView *)view {
     id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer]; // list of commands to the gpu
-    MTLRenderPassDescriptor *renderPassDescriptor = view.currentRenderPassDescriptor; // what pass i'm rendering to
 
+    // ---- compute pass ----
+    id<MTLComputeCommandEncoder> comp = [commandBuffer computeCommandEncoder];
+    [comp setComputePipelineState:_computePipeline];
+    [comp setTexture:_texture atIndex:0]; // index 0 matches index 0 in Shaders.metal
+    // launch one thread per pixel
+    MTLSize gridSize = MTLSizeMake(1600, 900, 1);
+    MTLSize threadgroupSize = MTLSizeMake(16, 16, 1); // idfk what these numbers are
+    [comp dispatchThreads:gridSize threadsPerThreadgroup:threadgroupSize];
+    [comp endEncoding];
+
+    // ---- render pass ----
+    MTLRenderPassDescriptor *renderPassDescriptor = view.currentRenderPassDescriptor; // what pass i'm rendering to
     if (renderPassDescriptor != nil) {
         id<MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
-        [renderEncoder setRenderPipelineState:_pipelineState];
-        [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3];
+        [renderEncoder setRenderPipelineState:_renderPipeline];
+        [renderEncoder setFragmentTexture:_texture atIndex:0]; // index 0 again matches index 0 in Shaders.metal
+        [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6];
         [renderEncoder endEncoding];
         // tell metal to display this frame's texture on screen when GPU finishes
         [commandBuffer presentDrawable:view.currentDrawable];
--- GPU_version/src/Shaders.metal
+++ GPU_version/src/Shaders.metal
@@ -1,30 +1,38 @@
 #include <metal_stdlib>
 using namespace metal;
 
+kernel void compute_test(
+	texture2d<float, access::write> output [[texture(0)]],
+	uint2 gid [[thread_position_in_grid]]) {
+	uint width = output.get_width();
+	uint height = output.get_height();
+	if (gid.x >= width || gid.y >= height) return;
+	float r = (float) gid.x / (float) width;
+	float g = (float) gid.y / (float) height;
+	output.write(float4(r, g, 0.0, 1.0), gid);
+}
+
 struct VertexOut {
     float4 position [[position]];
-    float4 color;
+    float2 uv;
 };
 
 vertex VertexOut vertex_main(uint vertexID [[vertex_id]]) {
-    float2 positions[3] = {
-        float2(0.0, 0.5),
-        float2(-0.5, -0.5),
-        float2(0.5, -0.5)
+    float2 positions[6] = {
+        float2(-1.0, -1.0),
+        float2( 1.0, -1.0),
+        float2(-1.0,  1.0),
+        float2(-1.0,  1.0),
+        float2( 1.0, -1.0),
+        float2( 1.0,  1.0),
     };
-
-    float4 colors[3] = {
-        float4(1.0, 0.0, 0.0, 1.0),
-        float4(0.0, 1.0, 0.0, 1.0),
-        float4(0.0, 0.0, 1.0, 1.0)
-    };
-
     VertexOut out;
     out.position = float4(positions[vertexID], 0.0, 1.0);
-    out.color = colors[vertexID];
+    out.uv = positions[vertexID] * 0.5 + float2(0.5);
     return out;
 }
 
-fragment float4 fragment_main(VertexOut in [[stage_in]]) {
-    return in.color;
+fragment float4 fragment_main(VertexOut in [[stage_in]], texture2d<float> texture [[texture(0)]]) {
+	constexpr sampler s(filter::linear);
+    return texture.sample(s, in.uv);
 }
